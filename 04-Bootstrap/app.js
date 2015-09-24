08830c848dbf87306b3502778511bc8c4a8b71be
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2015-09-23 20:38:40 -0600

    04 Bootstrap base

---
+++ 04-Bootstrap/app.js
@@ -0,0 +1,25 @@
+angular.module('bootstrap', ['ngRoute', 'bootstrap.controllers'])
+.config(['$routeProvider', function($routeProvider) {
+	
+	$routeProvider
+	
+	.when('/',{
+		templateUrl: 'views/home.html',
+		controller: 'HomeController'
+	})
+	
+	.when('/users',{
+		templateUrl: 'views/users.html',
+		controller: 'UsersController'
+	})
+	
+	.when('/products',{
+		templateUrl: 'views/products.html',
+		controller: 'ProductsController'
+	})
+	
+	.otherwise({
+		redirectTo: '/'
+	});
+	
+}]);
