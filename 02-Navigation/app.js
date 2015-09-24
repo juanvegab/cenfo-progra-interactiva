7bbc4b5838a5327b3c3dc44d2a5e23f4ed3b92e8
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2015-09-23 18:06:03 -0600

    02 - Navigation practice

--- 02-Navigation/app.js
+++ 02-Navigation/app.js
@@ -1,4 +1,19 @@
-angular.module('navigation', ['ngRoute', 'navigation.controllers'])
+angular.module('navigation', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-
+	$routeProvider
+	.when('/',{
+		templateUrl: 'views/home.html'
+	})
+	.when('/users',{
+		templateUrl: 'views/users.html'
+	})
+	.when('/products',{
+		templateUrl: 'views/products.html'
+	})
+	.when('/caca',{
+		templateUrl: 'views/caca.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 }]);
