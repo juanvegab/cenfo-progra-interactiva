2afb80c6b059a2ae071bd9eeaa3fca276c56a88d
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2015-11-18 18:02:56 -0600

    Chat component

---
+++ 17-chat/app.js
@@ -0,0 +1,4 @@
+// Declaracion de la aplicación Chat, aqui se define 
+// el nombre de la aplicacion y se incluyen las dependencias 
+// al archivo de controladores y el de las factories
+var app = angular.module('ChatUp', ['ChatUp.controllers', 'ChatUp.factories']);
