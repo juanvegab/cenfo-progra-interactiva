33f9b8497169a831b90a5fdb59639f6152e518e7
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2015-11-18 20:22:22 -0600

    removing extra files

--- examen-01/chat-group-juanvegab-polymer/app.js
+++
@@ -1,4 +0,0 @@
-// Declaracion de la aplicación Chat, aqui se define 
-// el nombre de la aplicacion y se incluyen las dependencias 
-// al archivo de controladores y el de las factories
-var app = angular.module('ChatUp', ['ChatUp.controllers', 'ChatUp.factories']);
